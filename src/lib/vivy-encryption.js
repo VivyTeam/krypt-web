73b91251900df92d9aa5e26e9359f7282706e1b2
paschalidi <paschalidi@outlook.com>
2019-01-25 14:44:28 +0100

    implements vivy-exncryption

---
+++ src/lib/vivy-encryption.js
@@ -0,0 +1,81 @@
+import {
+  generateInitialVector,
+  arrayBufferToString,
+  stringToArrayBuffer
+} from "./utilities";
+import {
+  generateKey as aesGenerateKey,
+  importKey as aesImportKey,
+  exportKey as aesExportKey,
+  encrypt as aesEncrypt,
+  decrypt as aesDecrypt
+} from "./aes-gcm";
+import { encrypt as rsaEncrypt, decrypt as rsaDecrypt } from "./rsa-oaep";
+
+/*
+  Vivy-Encryption - Encrypt
+  Encrypts aesKey and iv using RSA-OAEP to create a so called 'envelope'.
+  Encrypts data using AES-GCM.
+  Returns an object with the encrypted secrets (key,iv) and the data.
+*/
+export async function encrypt(publicKey, buffer) {
+  const iv = await generateInitialVector();
+  const aesKey = await aesGenerateKey();
+  const aesExportedKey = await aesExportKey(aesKey);
+  const ivBase64 = transformIvToBase64(iv);
+  const aesKeyBase64 = transformKeyToBase64(aesExportedKey);
+  const jsonString = JSON.stringify({
+    base64EncodedIV: ivBase64,
+    base64EncodedKey: aesKeyBase64
+  });
+  const bufferSecrets = stringToArrayBuffer(jsonString);
+
+  const cipherSecrets = await rsaEncrypt(publicKey, bufferSecrets);
+  const cipherData = await aesEncrypt(aesKey, iv, buffer);
+
+  return { cipherSecrets, cipherData, iv, aesExportedKey, aesKey };
+}
+
+/*
+   Vivy-Encryption - Decrypt
+*/
+export async function decrypt(privateKey, items) {
+  const { cipherSecrets, cipherData } = items;
+  const { key, iv } = await decryptCipherSecrets(privateKey, cipherSecrets);
+  return await decryptCipherData(key, iv, cipherData);
+}
+
+/*
+   Vivy-Encryption - Decrypts cipher secrets (key, iv) via RSA-OAEP.
+   Returned format is ArrayBuffer.
+*/
+async function decryptCipherSecrets(privateKey, cipherSecrets) {
+  const secretsArrayBuffer = await rsaDecrypt(privateKey, cipherSecrets);
+  const secretsJsonString = arrayBufferToString(secretsArrayBuffer);
+  const { base64EncodedKey, base64EncodedIV } = JSON.parse(secretsJsonString);
+  const key = stringToArrayBuffer(window.atob(base64EncodedKey));
+  const iv = stringToArrayBuffer(window.atob(base64EncodedIV));
+  return { key, iv };
+}
+
+/*
+   Vivy-Encryption - Decrypts cipher data via AES-GCM.
+   Returned format is ArrayBuffer.
+*/
+async function decryptCipherData(rawKey, arrayBufferIv, cipherData) {
+  const key = await aesImportKey(rawKey);
+  const iv = new Uint8Array(arrayBufferIv);
+
+  return await aesDecrypt(key, iv, cipherData);
+}
+
+function transformKeyToBase64(key) {
+  const string = arrayBufferToString(key);
+  return window.btoa(string);
+}
+
+function transformIvToBase64(iv) {
+  const buffer = iv.buffer;
+  const string = arrayBufferToString(buffer);
+  return window.btoa(string);
+}
---
+++ src/tests/vivy-encryption.test.js
@@ -0,0 +1,19 @@
+import { encrypt, decrypt } from "../../src/lib/vivy-encryption";
+import { generateKey } from "../../src/lib/rsa-oaep";
+import { arrayBufferToString, stringToArrayBuffer } from "../lib/utilities";
+
+describe("vivy-encryption", () => {
+  const expect = window.expect;
+
+  it("should encrypt data and decrypt it back.", async () => {
+    const { privateKey, publicKey } = await generateKey();
+    const originalString = "Encrypted secret message";
+    const buffer = stringToArrayBuffer(originalString);
+
+    const encryptedData = await encrypt(publicKey, buffer);
+    const arrayBufferData = await decrypt(privateKey, encryptedData);
+
+    const result = arrayBufferToString(arrayBufferData);
+    expect(result).to.deep.equal(originalString);
+  });
+});
