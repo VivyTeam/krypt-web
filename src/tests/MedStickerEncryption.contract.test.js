6f73c33ce0b76622deefbd804fe678ca417a3459
christos <paschalidi@outlook.com>
2019-03-11 18:08:38 +0100

    loops over tests

--- src/tests/MedStickerEncryption.contract.test.js
+++ src/tests/MedStickerEncryption.contract.test.js
@@ -13,101 +13,87 @@ import {
 } from "../lib/utilities";
 import { ADAM, BRITNEY } from "../lib/constants";
 
-it("ADAM: deriveKey value should match the value on the contract", async () => {
-  const code = "7i6XA2zz";
-  const pin = "qmHuG263";
-  const { key, iv } = deriveKey(code, pin, ADAM);
-  const givenKeyBase64 = "Pivil9wBlqECOP8qulkJnHFnIiIwSffQt4rXo27X4Uk=";
-  const givenIvBase64 = "gi44bZGuBBdLpMISpeppWQ==";
+const algorithms = [
+  {
+    name: ADAM,
+    encrypt: adamEncrypt,
+    code: "7i6XA2zz",
+    pin: "qmHuG263",
+    givenKeyBase64: "Pivil9wBlqECOP8qulkJnHFnIiIwSffQt4rXo27X4Uk=",
+    givenIvBase64: "gi44bZGuBBdLpMISpeppWQ==",
+    givenEncryptedDataBase64:
+      "rIfjcSAsEh/so+5+ijho97FmIRH36LCCkD/a0V0HWsmw01SEpxoYrQjp5Il5IITw"
+  },
+  {
+    name: BRITNEY,
+    encrypt,
+    code: "7i6XA2zz",
+    pin: "qmHuG263",
+    givenKeyBase64: "1v6YGdN6BW2AR1uEylOmjSwKu/kUr5qNYR42X0Che3U=",
+    givenIvBase64: "aoiywBzTwYxzKQz45UxWaQ==",
+    givenEncryptedDataBase64:
+      "1EkGWJAKP0BG2CAstCFcq8ysbOEvYwruJrrJUBRVGQMe8590wfdKge/jfKcLwEjFg7Q="
+  }
+];
 
-  const keyBase64 = arrayBufferToBase64(key);
-  const ivBase64 = arrayBufferToBase64(iv);
+describe("MedStickerEncryption contract", () => {
+  algorithms.forEach(algorithm => {
+    describe(`version ${algorithm.name}`, () => {
+      it("deriveKey value should match the value on the contract", async () => {
+        const { key, iv } = deriveKey(
+          algorithm.code,
+          algorithm.pin,
+          algorithm.name
+        );
 
-  expect(keyBase64).to.deep.equal(givenKeyBase64);
-  expect(ivBase64).to.deep.equal(givenIvBase64);
-});
+        const keyBase64 = arrayBufferToBase64(key);
+        const ivBase64 = arrayBufferToBase64(iv);
 
-it("ADAM: encrypted data should match the values on the contract", async () => {
-  const message = "A Healthier Life is a Happier Life";
-  const messageBuffer = stringToArrayBuffer(message);
-  const code = "7i6XA2zz";
-  const pin = "qmHuG263";
-  const { data } = await adamEncrypt(code, pin, messageBuffer);
-  const dataBase64 = arrayBufferToBase64(data);
+        expect(keyBase64).to.deep.equal(algorithm.givenKeyBase64);
+        expect(ivBase64).to.deep.equal(algorithm.givenIvBase64);
+      });
 
-  expect(dataBase64).to.deep.equal(
-    "rIfjcSAsEh/so+5+ijho97FmIRH36LCCkD/a0V0HWsmw01SEpxoYrQjp5Il5IITw"
-  );
-});
+      it("encrypted data should match the values on the contract", async () => {
+        const message = "A Healthier Life is a Happier Life";
+        const messageBuffer = stringToArrayBuffer(message);
+        const { data } = await algorithm.encrypt(
+          algorithm.code,
+          algorithm.pin,
+          messageBuffer
+        );
+        const dataBase64 = arrayBufferToBase64(data);
 
-it("ADAM: decrypted data should match the values on the contract", async () => {
-  const resultKeyBase64 = "Pivil9wBlqECOP8qulkJnHFnIiIwSffQt4rXo27X4Uk=";
-  const resultIvBase64 = "gi44bZGuBBdLpMISpeppWQ==";
-  const encryptedDataBase64 =
-    "rIfjcSAsEh/so+5+ijho97FmIRH36LCCkD/a0V0HWsmw01SEpxoYrQjp5Il5IITw";
+        expect(dataBase64).to.deep.equal(algorithm.givenEncryptedDataBase64);
+      });
 
-  const key = base64ToArrayBuffer(resultKeyBase64);
-  const iv = base64ToArrayBuffer(resultIvBase64);
-  const encryptedBuffer = base64ToArrayBuffer(encryptedDataBase64);
+      it("decrypted data should match the values on the contract", async () => {
+        const key = base64ToArrayBuffer(algorithm.givenKeyBase64);
+        const iv = base64ToArrayBuffer(algorithm.givenIvBase64);
+        const encryptedBuffer = base64ToArrayBuffer(
+          algorithm.givenEncryptedDataBase64
+        );
 
-  const data = await decrypt({ key, iv, version: ADAM }, encryptedBuffer);
-  const plainText = arrayBufferToString(data);
+        const data = await decrypt(
+          { key, iv, version: algorithm.name },
+          encryptedBuffer
+        );
+        const plainText = arrayBufferToString(data);
 
-  expect(plainText).to.deep.equal("A Healthier Life is a Happier Life");
-});
+        expect(plainText).to.deep.equal("A Healthier Life is a Happier Life");
+      });
+    });
+  });
 
-it("BRITNEY: deriveKey value should match the value on the contract", async () => {
-  const code = "7i6XA2zz";
-  const pin = "qmHuG263";
-  const { key, iv } = deriveKey(code, pin, BRITNEY);
-  const givenKeyBase64 = "1v6YGdN6BW2AR1uEylOmjSwKu/kUr5qNYR42X0Che3U=";
-  const givenIvBase64 = "aoiywBzTwYxzKQz45UxWaQ==";
-
-  const keyBase64 = arrayBufferToBase64(key);
-  const ivBase64 = arrayBufferToBase64(iv);
-
-  expect(keyBase64).to.deep.equal(givenKeyBase64);
-  expect(ivBase64).to.deep.equal(givenIvBase64);
-});
-
-it("BRITNEY: encrypted data should match the values on the contract", async () => {
-  const message = "A Healthier Life is a Happier Life";
-  const messageBuffer = stringToArrayBuffer(message);
-  const code = "7i6XA2zz";
-  const pin = "qmHuG263";
-  const { data } = await encrypt(code, pin, messageBuffer);
-  const dataBase64 = arrayBufferToBase64(data);
-
-  expect(dataBase64).to.deep.equal(
-    "1EkGWJAKP0BG2CAstCFcq8ysbOEvYwruJrrJUBRVGQMe8590wfdKge/jfKcLwEjFg7Q="
-  );
-});
-
-it("BRITNEY: decrypted data should match the values on the contract", async () => {
-  const resultKeyBase64 = "1v6YGdN6BW2AR1uEylOmjSwKu/kUr5qNYR42X0Che3U=";
-  const resultIvBase64 = "aoiywBzTwYxzKQz45UxWaQ==";
-  const encryptedDataBase64 =
-    "1EkGWJAKP0BG2CAstCFcq8ysbOEvYwruJrrJUBRVGQMe8590wfdKge/jfKcLwEjFg7Q=";
-
-  const key = base64ToArrayBuffer(resultKeyBase64);
-  const iv = base64ToArrayBuffer(resultIvBase64);
-  const encryptedBuffer = base64ToArrayBuffer(encryptedDataBase64);
-
-  const data = await decrypt({ key, iv, version: BRITNEY }, encryptedBuffer);
-  const plainText = arrayBufferToString(data);
-
-  expect(plainText).to.deep.equal("A Healthier Life is a Happier Life");
-});
-
-it("Signature should be equal as the contracts", async () => {
-  const code = "7i6XA2zz";
-  const pin = "qmHuG263";
-  const { key, iv } = deriveKey(code, pin, BRITNEY);
-  const signature = await accessSignature(
-    { key, iv, version: BRITNEY },
-    "98C1EB4EE93476743763878FCB96A25FBC9A175074D64004779ECB5242F645E6"
-  );
-  expect(signature).to.deep.equal(
-    "britney-sha256:RonmY2BVOex5wlGRrLPkXn/MZV1Rhot4wRc9+cuK0zY="
-  );
+  it("Signature should be equal as the contracts", async () => {
+    const code = "7i6XA2zz";
+    const pin = "qmHuG263";
+    const { key, iv } = deriveKey(code, pin, BRITNEY);
+    const signature = await accessSignature(
+      { key, iv, version: BRITNEY },
+      "98C1EB4EE93476743763878FCB96A25FBC9A175074D64004779ECB5242F645E6"
+    );
+    expect(signature).to.deep.equal(
+      "britney-sha256:RonmY2BVOex5wlGRrLPkXn/MZV1Rhot4wRc9+cuK0zY="
+    );
+  });
 });
