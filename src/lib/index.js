488bec4ce0b192ffb58382f5059c84c62580282c
christos <paschalidi@outlook.com>
2019-03-22 13:38:59 +0100

    reverts this

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,32 +1,8 @@
-import create from "./factory";
-import { decrypt as ehrDecrypt } from "./EHREncryption";
 import {
+  encrypt as medEncrypt,
   decrypt as medDecrypt,
   accessSignature as medAccessSignature,
   deriveKey as medDeriveKey
 } from "./MedStickerEncryption";
-import {
-  toPem,
-  toArrayBuffer,
-  arrayBufferToBase64,
-  base64ToArrayBuffer,
-  generateInitialVector,
-  pemToArrayBuffer
-} from "./utilities";
 
-export default {
-  create,
-
-  ehrDecrypt,
-
-  medDecrypt,
-  medAccessSignature,
-  medDeriveKey,
-
-  arrayBufferToBase64,
-  base64ToArrayBuffer,
-  toPem,
-  toArrayBuffer,
-  generateInitialVector,
-  pemToArrayBuffer
-};
+export default { medEncrypt, medDecrypt, medAccessSignature, medDeriveKey };
