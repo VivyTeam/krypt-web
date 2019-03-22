f26985ed8b17383e166336d5efdd567e43250cf9
christos <paschalidi@outlook.com>
2019-03-22 13:33:54 +0100

    adds airbnb-base fixes the rules according to react project applies feedback from eslint exports all the rules export utilities as well export create from factory export irt adds generateInitialVector toArrayBuffer arrayBufferToBase64 orders exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,32 @@
+import create from "./factory";
+import { decrypt as ehrDecrypt } from "./EHREncryption";
 import {
-  encrypt as medEncrypt,
   decrypt as medDecrypt,
   accessSignature as medAccessSignature,
   deriveKey as medDeriveKey
 } from "./MedStickerEncryption";
+import {
+  toPem,
+  toArrayBuffer,
+  arrayBufferToBase64,
+  base64ToArrayBuffer,
+  generateInitialVector,
+  pemToArrayBuffer
+} from "./utilities";
 
-export default { medEncrypt, medDecrypt, medAccessSignature, medDeriveKey };
+export default {
+  create,
+
+  ehrDecrypt,
+
+  medDecrypt,
+  medAccessSignature,
+  medDeriveKey,
+
+  arrayBufferToBase64,
+  base64ToArrayBuffer,
+  toPem,
+  toArrayBuffer,
+  generateInitialVector,
+  pemToArrayBuffer
+};
