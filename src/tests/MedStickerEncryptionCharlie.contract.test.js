1d41a3991928dca6255e28566a2fa0027efd7129
christos <paschalidi@outlook.com>
2019-06-26 16:05:12 +0200

    renames constants

--- src/tests/MedStickerEncryptionCharlie.contract.test.js
+++ src/tests/MedStickerEncryptionCharlie.contract.test.js
@@ -44,48 +44,48 @@ describe("test utilities ", () => {
 describe("MedStickerEncryption contract, version charlie", () => {
   // these all are contract values given from the other sources to validate the encryption,
   // therefore are shared between out tests
-  const originalString = "MyVeryImportantData";
-  const fakePin = "someRandomPin12345678";
-  const fakeBackendSecret = "someRandomBackendSecret";
-  const fakeSecondSalt = "someRandomSecondSalt";
-  const ivString = "AAAAAAAAAAAAAAAA";
-  const encryptedHex =
+  const ORIGINAL_STRING = "MyVeryImportantData";
+  const MOCK_PIN = "someRandomPin12345678";
+  const MOCK_SECRET = "someRandomBackendSecret";
+  const MOCK_SALT = "someRandomSecondSalt";
+  const MOCK_STRING_IV = "AAAAAAAAAAAAAAAA";
+  const HEX_ENCRYPTED =
     "70caa3b9cf21a495a0bb8269c3b7908fdbf26832a2d81bda1d686ed1212085270df3ba";
+  const HEX_256_FINGERPINT =
+    "charlie:21262f4bce9412a3321ef8511fc276d39075944fcec9806cdc1d9848d0db2c35";
+  const HEX_512_FINGERPINT =
+    "charlie:58c9d7da8beafcc58cd92e179e1276cd3a238d83265f82df11ead7585c86666c7a3b583c42ffab3c0eebf837bb841d44544289cd476770cc0c3be38974b04167";
 
-  const iv = stringToArrayBuffer(ivString);
-  const hashed = hash(fakePin + fakeBackendSecret, fakeSecondSalt);
+  const iv = stringToArrayBuffer(MOCK_STRING_IV);
+  const hashed = hash(MOCK_PIN + MOCK_SECRET, MOCK_SALT);
 
   it("should encrypt, transform result into hex, and match the contract's string", async () => {
     const { key } = splitKeys(hashed);
     const cryptoKey = await gcm.importKey(key);
-    const bytesToEncrypt = stringToArrayBuffer(originalString);
+    const bytesToEncrypt = stringToArrayBuffer(ORIGINAL_STRING);
 
     const encryptedBytes = await encrypt(cryptoKey, iv, bytesToEncrypt);
 
-    expect(arrayBufferToHex(encryptedBytes)).toEqual(encryptedHex);
+    expect(arrayBufferToHex(encryptedBytes)).toEqual(HEX_ENCRYPTED);
   });
 
   it("should decrypt, transform result into ArrayBuffer, and match the contract's string", async () => {
     const { key } = splitKeys(hashed);
     const cryptoKey = await gcm.importKey(key);
-    const encryptedBytes = hexToArrayBuffer(encryptedHex);
+    const encryptedBytes = hexToArrayBuffer(HEX_ENCRYPTED);
 
     const arrayBufferData = await decrypt(cryptoKey, iv, encryptedBytes);
 
-    expect(arrayBufferToString(arrayBufferData)).toEqual(originalString);
+    expect(arrayBufferToString(arrayBufferData)).toEqual(ORIGINAL_STRING);
   });
 
   it("should split the result of the hash and compare it with the contract's string", () => {
     const { fingerprintFile } = splitKeys(hashed);
-    expect(fingerprintFile).toEqual(
-      "charlie:21262f4bce9412a3321ef8511fc276d39075944fcec9806cdc1d9848d0db2c35"
-    );
+    expect(fingerprintFile).toEqual(HEX_256_FINGERPINT);
   });
 
   it("should compare fingerprint with contract's string. Salt should be CHARLIE_STATIC_SALT", () => {
-    const secret = hash(fakePin, CHARLIE_STATIC_SALT);
-    expect(fingerprint(secret)).toEqual(
-      "charlie:58c9d7da8beafcc58cd92e179e1276cd3a238d83265f82df11ead7585c86666c7a3b583c42ffab3c0eebf837bb841d44544289cd476770cc0c3be38974b04167"
-    );
+    const secret = hash(MOCK_PIN, CHARLIE_STATIC_SALT);
+    expect(fingerprint(secret)).toEqual(HEX_512_FINGERPINT);
   });
 });
